2c285bde0ea71f7feb56b3113988c99cebe4cfd0
mateo correa <ma.correa14@ciaf.edu.co>
2025-10-17 16:04:39 -0500

    ejercicios 6 y 8 adelantados

---
+++ ejercicios/ejercicio-08.js
@@ -0,0 +1,11 @@
+const num1 = 10
+const num2 = 5
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+console.log(`suma: ${suma}, resta: ${resta}, multiplicacion: ${multiplicacion}, division: ${division}`)
+
+console.log(num2 / 0)
